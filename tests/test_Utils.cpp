76b7b1c5ac7bdd3f2b86f7a8224ba0c12a5e0e2f
Dmitrij Kosmakov <digital.artificial@gmail.com>
2020-05-05 21:24:37 +0300

    Fix min/max heap test

--- tests/test_Utils.cpp
+++ tests/test_Utils.cpp
@@ -5,15 +5,19 @@
 
 using namespace Utils;
 
-
-TEST_CASE( "Utils: priority_queue", "[priority_queue]" )
+TEST_CASE( "Utils: priority_queue/max", "[priority_queue]" )
 {
-	std::priority_queue<int> maxq;
+	std::priority_queue<int/*, std::vector<int>, std::greater<int>*/> maxq;
 	maxq.push( 2 );
 	maxq.push( 1 );
 	maxq.push( 3 );
-
 	CHECK( maxq.top() == 3 );
-
+}
+TEST_CASE( "Utils: priority_queue/min", "[priority_queue]" )
+{
 	std::priority_queue<int, std::vector<int>, std::greater<int>> minq;
+	minq.push( 2 );
+	minq.push( 1 );
+	minq.push( 3 );
+	CHECK( minq.top() == 1 );
 }
